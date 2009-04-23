3428bd62faf73b011fea873504c6ea1c9fa54591
drevildeveloper <drevildeveloper@720dfec8-8478-4fe4-81a3-4ee7229e1367>
2009-04-23 07:58:30 +0000

    experimental enet stuff

---
+++ gmsrc_ex/src/binds/gmEnet.h
@@ -0,0 +1,8 @@
+
+#ifndef _GM_ENET_H_
+#define _GM_ENET_H_
+
+class gmMachine;
+void gmBindEnet(gmMachine *a_machine);
+
+#endif
---
+++ gmsrc_ex/src/binds/gmEnetImpl.h
@@ -0,0 +1,8 @@
+
+#ifndef _GM_ENET_IMPL_H_
+#define _GM_ENET_IMPL_H_
+
+bool CreateHost(const char *a_ip = 0, unsigned short a_port = 1234, int a_maxclients = 32);
+
+#endif
+
