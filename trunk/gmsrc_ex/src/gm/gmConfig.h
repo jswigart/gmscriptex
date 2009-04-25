fcb85baa272d479a3e6ee233a31718738cdbaf16
drevildeveloper <drevildeveloper@720dfec8-8478-4fe4-81a3-4ee7229e1367>
2009-04-25 18:04:04 +0000

    switch statement support, courtesy of downgraded

--- trunk/gmsrc_ex/src/gm/gmConfig.h
+++ trunk/gmsrc_ex/src/gm/gmConfig.h
@@ -94,9 +94,6 @@ enum gmEndian
 
 #define GMMACHINE_CPPOWNEDGMOBJHASHSIZE 1024  // default hash table size for objects owned by cpp code, necessary for GC.
 
-#define GM_USE_VECTOR3_STACK		1
-#define GM_USE_ENTITY_STACK			1
-
 // DEBUGGING
 
 #define GMDEBUG_SUPPORT             1         // allow use with the gm debugger
@@ -105,9 +102,12 @@ enum gmEndian
 // GARBAGE COLLECTOR
 #define GM_USE_INCGC                1         // use incremental garbage collector
 
+#define GM_USE_VECTOR3_STACK	    1         // Support for stack based vector3 type
+#define GM_USE_ENTITY_STACK	    1         // Support for stack based entity handle(as an int)
 
 #define GM_BOOL_OP                  1         // Spport for a bool operator on user types for use in if statements. For full effect, users will want to implement operators [bool, ==, !=, !]
 #define GM_USE_FORK                 1         // Support fork instruction 
 #define GM_USER_FOREACH             1         // Support foreach for user types
+#define GM_USE_SWITCH		    1	      // Support switch statements
 
 #endif // _GMCONFIG_H_
