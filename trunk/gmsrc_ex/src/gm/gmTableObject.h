639e333e619cbbbea28d6f740b144e5a395b5709
crapshoot <crapshoot@720dfec8-8478-4fe4-81a3-4ee7229e1367>
2011-10-06 19:34:11 +0000

    merged updates from gm versions 1_28_2 and 1_28_3 still an issue with 64bit and NullifyObject (equality assert fails)

--- trunk/gmsrc_ex/src/gm/gmTableObject.h
+++ trunk/gmsrc_ex/src/gm/gmTableObject.h
@@ -120,7 +120,7 @@ private:
 
 	void Construct(gmMachine * a_machine);
 
-	
+	// This is a candidate for optimization. May want to specialize hash for variable types and platform.
 	inline gmTableNode * GetAtHashPos(const gmVariable* a_key) const
 	{
 		unsigned int hash = (unsigned int)a_key->m_value.m_ref; // Use lower 32 bits for now (NOTE: Alternate hashing method may improve performance)
