df6e17d55a3cde4609487639d720cf7cb1f2eebe
crapshoot <crapshoot@720dfec8-8478-4fe4-81a3-4ee7229e1367>
2012-05-15 18:41:10 +0000

    funkbot's fix for Fork stack growth bug

--- gmsrc_ex/src/gm/gmThread.cpp
+++ gmsrc_ex/src/gm/gmThread.cpp
@@ -835,7 +835,8 @@ gmThread::State gmThread::Sys_Execute(gmVariable * a_return)
 				GM_ASSERT( newthr );
 
 				// make sure there is enough room
-				newthr->Touch( m_size - m_base + 2 );
+				// http://www.gmscript.com/gamemonkey/forum/viewtopic.php?f=6&t=603
+				newthr->Touch( m_size - m_base + 2 - GMTHREAD_SLACKSPACE);
 				// copy stack and vars
 				memcpy( newthr->m_stack, &m_stack[ m_base - 2 ], sizeof( gmVariable ) * (m_top - m_base + 2 ) );
 
